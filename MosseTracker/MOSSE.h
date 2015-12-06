888b69abd54505ccad8ae81fce3ba279bd3a9034
Wang Wenda <wwd7086@gmail.com>
2015-12-06 13:52:12 -0500

    vdsp works

--- MosseTracker/MOSSE.h
+++ MosseTracker/MOSSE.h
@@ -2,48 +2,69 @@
 #define MOSSE_H
 
 #include <opencv2/opencv.hpp>
+#include <Accelerate/Accelerate.h>
 
 class MOSSE{
 public:
     
     // constructor
     MOSSE(cv::Mat& frame, cv::Rect rect);
+    ~MOSSE();
+    
     // call it every frame
-    void update(cv::Mat& frame, double rate=0.125);
-    // return center
+    void update(cv::Mat& frame, float rate=0.125);
+    
+    // return center of bounding box
     cv::Point getCenter() {
         return pos;
     }
+    // retrun bounding box
     cv::Rect getRect() {
         return cv::Rect(pos.x-size.width/2,pos.y-size.height/2,
                         size.width,size.height);
     }
     
-    // some visualization TODO
-    cv::Mat state_vis();
-    void draw_state(cv::Mat& vis);
-    
 private:
     
     void preprocess(cv::Mat& img);
-    double correlate(cv::Mat& img, cv::Mat& last_resp, cv::Point& delta_xy);
+    double correlate(cv::Mat& img, cv::Point& delta_xy);
     void update_kernel();
-    void shiftRows(cv::Mat& mat);
-    void shiftRows(cv::Mat& mat,int n);
-    void shiftCols(cv::Mat& mat, int n);
     
-    
+    float* createDSPSplitComplex(DSPSplitComplex& splitComplex);
+    void doFFT(cv::Mat& mat, DSPSplitComplex* splitComplex);
+    void doMulti(DSPSplitComplex* a, DSPSplitComplex* b, DSPSplitComplex* c);
+    void doDivide(DSPSplitComplex* a, DSPSplitComplex* b, DSPSplitComplex* c);
+    void doScale(DSPSplitComplex* a, float scale);
+
     // the size of bounding box
     cv::Size size;
+    int wlog2;
+    int hlog2;
+    unsigned long totalSize;
     // the center point of bounding box
     cv::Point pos;
     
-    // ground truth response in frequency
-    cv::Mat G;
-    // filter in frequency
-    cv::Mat H1;
-    cv::Mat H2;
-    cv::Mat H;
+    // ground truth response
+    DSPSplitComplex G;
+    float* G_M;
+    // filter
+    DSPSplitComplex H1;
+    float* H1_M;
+    DSPSplitComplex H2;
+    float* H2_M;
+    DSPSplitComplex H;
+    float* H_M;
+    // temp filter
+    DSPSplitComplex H1t;
+    float* H1t_M;
+    DSPSplitComplex H2t;
+    float* H2t_M;
+    // temp sub image
+    DSPSplitComplex I;
+    float* I_M;
+    // temp filter response
+    DSPSplitComplex R;
+    float* R_M;
     
     // hannng window for preprocessing
     cv::Mat win;
@@ -63,6 +84,9 @@ private:
     const float eps = 0.00001;
     const float boundaryThre = 0.25;
     const int updateThre = 4;
+    
+    // FFT vdsp
+    FFTSetup FFTSetup;
 };
 
 #endif
